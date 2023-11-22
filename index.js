37f98c725ddd4a014395a2498a752b33ac643420
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-11-22 08:13:30 -0500

    temp

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ app.get('/products/:id', async (req, res) => {
 })
 
 app.post('/products', async (req, res) => {
-  l(req.body)
   const newProduct = new Product(req.body)
   await newProduct.save()
   res.redirect(`/products`)
