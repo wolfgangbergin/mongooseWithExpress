c2d95cef67eacce750964af875b839700b305dca
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-12-06 06:57:50 -0500

    temp

--- temp5.js
+++ temp5.js
@@ -5,19 +5,19 @@ const array1 = [
 ]
 
 function getPosts() {
-    return new Promise((res, rej) => {
-  let output = ``
-  let array2 = ['apple']
-  setTimeout(() => {
-    array1.forEach((post) => {
-      l('loop')
-      array2.push(post.fName)
-      output += post.fName + '\n'
-    })
+  return new Promise((res, rej) => {
+    let output = ``
+    let array2 = ['apple']
+    setTimeout(() => {
+      array1.forEach((post) => {
+        l('loop')
+        array2.push(post.fName)
+        output += post.fName + '\n'
+      })
       // console.log(output)
-  }, 500)
-  res(array2)
-})
+    }, 500)
+    res(array2)
+  })
 }
 
 const createPosts = (post) => {
@@ -32,32 +32,32 @@ const createPosts = (post) => {
   })
 }
 
-
-
-
-
-
-
-
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
 async function init() {
-    const proimse1 = Promise.resolve('hello world')
-const proimse2 = 10
-const proimse3 = new Promise((res, rej) => {
-  setTimeout(res, 2000, 'goodbye')
-})
+  const proimse1 = Promise.resolve('hello world')
+  const proimse2 = 10
+  const proimse3 = new Promise((res, rej) => {
+    setTimeout(res, 2000, 'goodbye')
+  })
 
-    const proimse4 = fetch('https://jsonplaceholder.typicode.com/users')
-    .then((res) => res.json())
-    .then((data) => data[1].name)
+  const temp1 = await fetch('https://jsonplaceholder.typicode.com/users')
+  const temp2 = await temp1.json()
+  const proimse4 = temp2[2].name
 
-    await createPosts({ fName: 'bobo2', lName: 'bobo2' })
-    
-    const proimse5 = await getPosts()
-    l(proimse5)
-  Promise.all([proimse1, proimse2, proimse3, proimse4, proimse5]).then(
-    (values) => l(values)
-  )
+  await createPosts({ fName: 'bobo2', lName: 'bobo2' })
+
+  const proimse5 = await getPosts()
+
+  l(proimse5)
+
+  const Result = await Promise.all([
+    proimse1,
+    proimse2,
+    proimse3,
+    proimse4,
+    proimse5,
+  ])
+  l(Result)
 }
 
 init()
