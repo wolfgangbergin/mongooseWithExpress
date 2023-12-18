49743e6db629343a0c91ab732891b3c71bcd4e0a
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-12-18 07:30:48 -0500

    temp

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const methodOverride = require('method-override')
 const asyncError = require('./asyncError/asyncError')
 const ExpressError = require('./utils/ExpressError')
 const ejsMate = require('ejs-mate')
-const kimbo = require('./routes/farms')
+const farmRoutes = require('./routes/farms')
 
 
 mongoose
@@ -30,122 +30,11 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-// wolf.Farm routes
-
-app.use('/farms', kimbo)
-
-// app.get(
-//   '/farms',
-//   asyncError(async (req, res, next) => {
-//     const farms = await wolf.Farm.find({})
-//     res.render('farms/index', { farms })
-    
-//   })
-// )
 
 
+app.use('/farms', farmRoutes)
 
 
-// app.get('/farms/new', (req, res, next) => {
-//   res.render('farms/new')
-// })
-
-
-// app.post(
-//   '/farms',
-//   asyncError(async (req, res, next) => {
-//     l(`req.body`, req)
-//     const newFarm = new wolf.Farm(req.body)
-//     await newFarm.save()
-//     res.redirect(`/farms`)
-//   })
-// )
-
-// app.post(
-//   '/farms/:_id',
-//   asyncError(async (req, res, next) => {
-    
-//     const newID = await wolf.deleteAndReplace(
-//       req,
-//       res,
-//       next,
-//     )
-   
-    
-//     res.redirect(`/farms/${newID}`)
-//   })
-// )
-
-
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
-
-// app.get(
-//   '/farms/:_id/delete',
-//   asyncError(async (req, res, next) => {
-//     const oldFarm = await wolf.Farm.findByIdAndDelete(
-//       req.params._id
-//     )
-
-//     await wolf.Product.deleteMany({
-//       _id: { $in: oldFarm.products },
-//     })
-
-//     res.redirect(`/farms`)
-//   })
-// )
-
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
-
-// app.get(
-//   '/farms/:_id/edit',
-//   asyncError(async (req, res, next) => {
-//     const farm = await wolf.Farm.findById(
-//       req.params._id
-//     )
-//     res.render('farms/edit', { farm })
-//   })
-// )
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
-
-// app.get(
-//   '/farms/:_id',
-//   asyncError(async (req, res, next) => {
-//     const farm = await wolf.Farm.findById(
-//       req.params._id
-//     ).populate('products')
-
-//     res.render('farms/show', { farm })
-//   })
-// )
-
-// app.get(
-//   '/farms/:_id/products/new',
-//   asyncError(async (req, res) => {
-//     const farm = await wolf.Farm.findById(
-//       req.params._id
-//     )
-//     res.render('farms/newFarmProduct.ejs', { farm })
-//   })
-// )
-
-// app.post(
-//   '/farms/:_id/products',
-//   asyncError(async (req, res, next) => {
-//     const farm = await wolf.Farm.findById(
-//       req.params._id
-//     )
-//     const newProduct = new Product({ ...req.body })
-//     farm.products.push(newProduct)
-//     newProduct.farm = farm
-//     await newProduct.save()
-//     await farm.save()
-//     res.redirect(`/farms/${farm._id}`)
-//   })
-// )
-
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
 
 // product routes
 
